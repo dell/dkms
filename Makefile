dell/dkms

commit 48f81691c2c774becbb4fbfff826e44bc1e7dea1

Drop distro-specific packaging

Makefile
@@ -26,7 +26,7 @@ TOPDIR := $(shell pwd)
 
 .PHONY = tarball
 
-all: clean tarball rpm debs
+all: clean tarball
 
 clean:
 	-rm -rf *~ dist/
@@ -86,12 +86,11 @@ install-debian: install install-doc
 	rm $(DOCDIR)/COPYING*
 	rm $(DOCDIR)/sample*
 
-deb_destdir=$(BUILDDIR)/dist
-TARBALL=$(deb_destdir)/$(RELEASE_STRING).tar.gz
+TARBALL=$(BUILDDIR)/dist/$(RELEASE_STRING).tar.gz
 tarball: $(TARBALL)
 
 $(TARBALL):
-	mkdir -p $(deb_destdir)
+	mkdir -p $(BUILDDIR)/dist
 	tmp_dir=`mktemp -d --tmpdir dkms.XXXXXXXX` ; \
 	cp -a ../$(RELEASE_NAME) $${tmp_dir}/$(RELEASE_STRING) ; \
 	sed -e "s/#RELEASE_VERSION#/$(RELEASE_VERSION)/" dkms > $${tmp_dir}/$(RELEASE_STRING)/dkms ; \
@@ -105,34 +104,3 @@ $(TARBALL):
 	sync ; sync ; sync ; \
 	tar cvzf $(TARBALL) -C $${tmp_dir} $(RELEASE_STRING); \
 	rm -rf $${tmp_dir} ;
-
-
-rpm: $(TARBALL) dkms.spec
-	tmp_dir=`mktemp -d --tmpdir dkms.XXXXXXXX` ; \
-	echo $(tmp_dir); \
-	mkdir -p $${tmp_dir}/{BUILD,RPMS,SRPMS,SPECS,SOURCES} ; \
-	cp $(TARBALL) $${tmp_dir}/SOURCES ; \
-	sed "s/#RELEASE_VERSION#/$(RELEASE_VERSION)/" dkms.spec > $${tmp_dir}/SPECS/dkms.spec ; \
-	pushd $${tmp_dir} > /dev/null 2>&1; \
-	rpmbuild -ba --define "_topdir $${tmp_dir}" SPECS/dkms.spec ; \
-	popd > /dev/null 2>&1; \
-	cp $${tmp_dir}/RPMS/noarch/* $${tmp_dir}/SRPMS/* dist ; \
-	rm -rf $${tmp_dir}
-
-debmagic: $(TARBALL)
-	mkdir -p dist/
-	ln -s $(TARBALL) $(DEB_TMP_BUILDDIR)/$(RELEASE_NAME)_$(RELEASE_VERSION).orig.tar.gz
-	tar -C $(DEB_TMP_BUILDDIR) -xzf $(TARBALL)
-	cp -ar debian $(DEB_TMP_BUILDDIR)/$(RELEASE_STRING)/debian
-	chmod +x $(DEB_TMP_BUILDDIR)/$(RELEASE_STRING)/debian/rules
-	cd $(DEB_TMP_BUILDDIR)/$(RELEASE_STRING) ; \
-	dch -v $(RELEASE_VERSION)-0 "New upstream version, $(RELEASE_VERSION)"; \
-	dpkg-buildpackage -D -b -rfakeroot ; \
-	dpkg-buildpackage -D -S -sa -rfakeroot ; \
-	mv ../$(RELEASE_NAME)_* $(TOPDIR)/dist/ ; \
-	cd -
-
-debs:
-	tmp_dir=`mktemp -d --tmpdir dkms.XXXXXXXX` ; \
-	make debmagic DEB_TMP_BUILDDIR=$${tmp_dir} DIST=$(DIST); \
-	rm -rf $${tmp_dir}

debian/HOWTO.Debian
@@ -1,40 +1,0 @@
-HOWTO Build DKMS debs w/ Debian & Ubuntu systems
-Copyright 2008 Dell Inc.
- Author: Mario Limonciello <Mario_Limonciello@Dell.com>
-
-------------
-
-A dkms deb is a common representation of a DKMS package that can be distributed across multiple machines.
-After you have a functional DKMS package, you can follow these steps to build a DKMS deb.
-
-1) Start out by putting together a tree in /usr/src/PACKAGE-VERSION.  For our example, we are going to use
-the PACKAGE lirc and VERSION 0.8.3~pre1.  Create a dkms.conf per the recommendations in the DKMS guide.
-
-2) "Add" the package to the dkms tree system:
-	dkms add -m lirc -v 0.8.3~pre1
-
-3) "Build" the binary modules for the current kernel:
-	dkms build -m lirc -v 0.8.3~pre1
-
-4) Make the deb and/or dsc for the package:
-	dkms mkdeb -m lirc -v 0.8.3~pre1
-	dkms mkdsc -m lirc -v 0.8.3~pre1
-
-The end result will be a deb and/or dsc in /var/lib/dkms/lirc/0.8.3~pre1/deb or /var/lib/dkms/lirc/0.8.3~pre1/dsc
-
-------------
-If you would prefer not to contaminate your system with the changes from building these debs, you should
-create a framework.conf that contains these variables (adjust for your use case):
-
-	source_tree="/path/to/source/tree"
-	dkms_tree="/path/to/dkms/tree"
-
-You will then call DKMS with an extra variable, '--dkmsframework'.  Here is an example of how this would work
-using the same lirc example above:
-
-	dkms add -m lirc -v 0.8.3~pre1 --dkmsframework framework.conf
-	dkms build -m lirc -v 0.8.3~pre1 --dkmsframework framework.conf
-	dkms mkdeb -m lirc -v 0.8.3~pre1 --dkmsframework framework.conf
-	dkms mkdsc -m lirc -v 0.8.3~pre1 --dkmsframework framework.conf
-
-The end result will be both a debian binary package and a debian source package that you can use.

debian/compat
@@ -1,1 +1,0 @@
-7

debian/control
@@ -1,28 +1,0 @@
-Source: dkms
-Section: kernel
-Priority: optional
-Maintainer: Dynamic Kernel Modules Support Team <pkg-dkms-maint@lists.alioth.debian.org>
-Uploaders: David Paleino <dapal@debian.org>,
- Mario Limonciello <Mario_Limonciello@dell.com>,
- Giuseppe Iuculano <iuculano@debian.org>
-Build-Depends: debhelper (>= 7.0.50~), quilt
-Standards-Version: 3.9.2
-Homepage: https://github.com/dell-oss/dkms
-Vcs-Git: https://github.com/dell-oss/dkms.git
-Vcs-Browser: https://github.com/dell-oss/dkms.git
-
-Package: dkms
-Architecture: all
-Depends: ${misc:Depends},
- kmod | module-init-tools,
- gcc,
- make | build-essential | dpkg-dev,
- coreutils (>= 7.4),
- patch
-Recommends: fakeroot,
- menu | sudo
-Suggests: dpkg-dev, debhelper
-Description: Dynamic Kernel Module Support Framework
- DKMS is a framework designed to allow individual kernel modules to be upgraded
- without changing the whole kernel. It is also very easy to rebuild modules as
- you upgrade kernels.

debian/copyright
@@ -1,24 +1,0 @@
-Source: https://github.com/dell/dkms
-
-Files: debian/HOWTO.Debian
-Copyright: © 2008-2016, Dell, Inc. <dkms-devel@lists.us.dell.com>
-
-License: GPL-2+
- This package is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
-Files: debian/*
-Copyright: © 2008-2009, David Paleino <d.paleino@gmail.com>
-           © 2005-2008, Matt Domsch <Matt_Domsch@dell.com>
-           © 2008-2009, Mario Limonciello <Mario_Limonciello@dell.com>
-           © 2008-2009, Giuseppe Iuculano <giuseppe@iuculano.it>
-License: GPL-2+
-
-Files: *
-Copyright: © 2003-2009, Dell, Inc. <dkms-devel@lists.us.dell.com>
-License: GPL-2+
-
-On Debian systems, the complete text of the GNU General
-Public License v2 can be found in `/usr/share/common-licenses/GPL-2'.

debian/dkms.lintian-overrides
@@ -1,1 +1,0 @@
-dkms binary: python-script-but-no-python-dep usr/share/apport/package-hooks/dkms_packages.py

debian/examples
@@ -1,4 +1,0 @@
-sample.conf
-sample.spec
-sample-suse-10-mkkmp.spec
-sample-suse-9-mkkmp.spec

debian/install
@@ -1,4 +1,0 @@
-debian/scripts/dh_dkms      usr/bin
-debian/scripts/dh_dkms.1    usr/share/man/man1/
-debian/scripts/*-dkms       usr/share/debhelper/autoscripts/
-debian/scripts/dkms.pm      usr/share/perl5/Debian/Debhelper/Sequence/

commit 9af400e616b6019158080216f299f37322b4b1ac

make: remove dkms.spec reference
Cc: Simone Caronni <negativo17@gmail.com>
Fixes: 48f8169 ("Drop distro-specific packaging")
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

Makefile
@@ -93,7 +93,6 @@ $(TARBALL):
 	tmp_dir=`mktemp -d --tmpdir dkms.XXXXXXXX` ; \
 	cp -a ../$(RELEASE_NAME) $${tmp_dir}/$(RELEASE_STRING) ; \
 	sed -e "s/#RELEASE_VERSION#/$(RELEASE_VERSION)/" dkms > $${tmp_dir}/$(RELEASE_STRING)/dkms ; \
-	sed -e "s/#RELEASE_VERSION#/$(RELEASE_VERSION)/" dkms.spec > $${tmp_dir}/$(RELEASE_STRING)/dkms.spec ; \
 	find $${tmp_dir}/$(RELEASE_STRING) -depth -name .git -type d -exec rm -rf \{\} \; ; \
 	find $${tmp_dir}/$(RELEASE_STRING) -depth -name dist -type d -exec rm -rf \{\} \; ; \
 	find $${tmp_dir}/$(RELEASE_STRING) -depth -name \*~ -type f -exec rm -f \{\} \; ; \