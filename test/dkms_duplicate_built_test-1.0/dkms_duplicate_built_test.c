0b87b7770b26742d9151c0d4c4b36ed3e90ffcc5
Andreas Beckmann <anbe@debian.org>
2025-03-07 10:54:02 +0100

    test dkms.conf specifying a module twice in BUILT_MODULE_NAME[]

---
+++ test/dkms_duplicate_built_test-1.0/dkms_duplicate_built_test.c
@@ -0,0 +1,23 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#define  DKMS_TEST_VER "1.0"
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("A Simple dkms test module");
+
+static int __init dkms_test_init(void)
+{
+    printk(KERN_INFO "DKMS Test Module -%s Loaded\n",DKMS_TEST_VER);
+    return 0;
+}
+
+static void __exit dkms_test_cleanup(void)
+{
+    printk(KERN_INFO "Cleaning up after dkms test module.\n");
+}
+
+module_init(dkms_test_init);
+module_exit(dkms_test_cleanup);
+MODULE_VERSION(DKMS_TEST_VER);
